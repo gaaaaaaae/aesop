1c22bc5cf82bbdff7bb884ec9e198943588498d6
gaaaaaaae <oga56@naver.com>
2022-07-05 15:24:09 +0900

    1.4

--- js/script_sub.js
+++ js/script_sub.js
@@ -14,34 +14,33 @@ $(function(){
   });
 
   //pairing 드래그
-  //#bar를 x축으로만 드래그 할 수 있도록 한다. 그 범위는 #bar의 부모크기 만큼만 가능
   $(".perfumeDrag").on("mousewheel",function(e){
-    e.preventDefault();//window의 마우스휠이벤트를 제거
-    var delta = e.originalEvent.deltaY //휠을 위로 올리면 -100반환, 밑으로 내리면 100을 반환
+    e.preventDefault();
+    var delta = e.originalEvent.deltaY
     var drag = document.querySelector(".perfumeDrag");
-    var pos = drag.scrollLeft //.perfumeDrag요소의 스크롤 위치값을 구함
-    if(delta > 0){//휠을 밑으로 내렸을 경우
+    var pos = drag.scrollLeft
+    if(delta > 0){
       pos += 40; 
-      drag.scrollLeft = pos//drag요소의 스크롤을 40px만큼 이동시키는 코드
-    }else{//휠을 위로 올렸을 경우
+      drag.scrollLeft = pos
+    }else{
       pos -= 40
-      drag.scrollLeft = pos//drag요소의 스크롤을 -40px만큼 이동시키는 코드
+      drag.scrollLeft = pos
     }
   });
 
   //window객체에 스크롤이벤트 발생시
-  $(window).scroll(function(){//window객체에 스크롤 이벤트 발생
-    var scroll = $(window).scrollTop();//변수 scroll에 현재 스크롤한 만큼의 거리를 저장
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
     console.log(scroll);
-    if(scroll > 20){//스크롤이동값이 20보다 클때 구문실행
+    if(scroll > 20){
       $(".topbtn").css("display","block");
-    }else{//스크롤이동값이 20보다 작을때 구문실행
-      $(".topbtn").css("display","none");//버튼요소를 화면에서 제거
+    }else{
+      $(".topbtn").css("display","none");
     }
   });
 
   //top버튼 클릭시
   $(".topbtn").click(function(){
-    $(window).scrollTop(0);//window객체의 스크롤위치값을 페이지상단으로 설정
+    $(window).scrollTop(0);
   })
 });
